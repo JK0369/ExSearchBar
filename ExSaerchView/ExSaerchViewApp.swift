697b6d912730941ae3dddd40149e1f54e37fb726
jake <palatable7@naver.com>
2022-09-08 22:05:02 +0900

    Initial Commit

---
+++ ExSaerchView/ExSaerchViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExSaerchViewApp.swift
+//  ExSaerchView
+//
+//  Created by 김종권 on 2022/09/08.
+//
+
+import SwiftUI
+
+@main
+struct ExSaerchViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
