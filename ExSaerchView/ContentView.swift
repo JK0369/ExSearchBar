e5dfd4cbbb493b07785b54ce5f39f96c2054ccaa
jake <palatable7@naver.com>
2022-09-08 23:44:20 +0900

    Implement

--- ExSaerchView/ContentView.swift
+++ ExSaerchView/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State var searchQueryString = ""
+  var datas = (0...100).map(String.init).map(SomeData.init)
+  var filteredDatas: [SomeData] {
+    if searchQueryString.isEmpty {
+      return datas
+    } else {
+      return datas.filter { $0.name.localizedStandardContains(searchQueryString) }
     }
+  }
+  
+  var body: some View {
+    NavigationView {
+      List(filteredDatas) { data in
+        NavigationLink {
+          SomeView(name: data.name)
+        } label: {
+          Text(data.name)
+        }
+      }
+      .listStyle(.plain)
+      .navigationTitle("Search Test")
+    }
+    .searchable(
+      text: $searchQueryString,
+      placement: .navigationBarDrawer(displayMode: .always),
+      prompt: "검색 placholder..."
+    )
+    .onSubmit(of: .search) {
+      print("검색 완료: \(searchQueryString)")
+    }
+    .onChange(of: searchQueryString) { newValue in
+      // viewModel 사용 시 이곳에서 새로운 값 입력
+      print("검색 입력: \(newValue)")
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
+}
+
+struct SomeView: View {
+  var name: String
+  
+  var body: some View {
+    Text(name)
+  }
+}
+
+struct SomeData: Identifiable {
+  var name: String
+  var id: String { self.name }
 }
